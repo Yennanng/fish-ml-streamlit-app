f417227f3280181aa3ac7265aee3b3778717b5df
Yennanng <yennan275@gmail.com>
2025-06-03 11:24:19 +0700

    uuu

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import io
 
 # --- THÔNG TIN CẦN THAY ĐỔI ---
 # Dán Webhook Test URL mà bạn đã sao chép từ n8n ở Bước 2 vào đây
-N8N_WEBHOOK_URL = "https://n8n.n2nai.io/webhook-test/64c7e9e9-5e9c-4ed7-b161-df272b164bf5" # THAY THẾ XXXXXX BẰNG URL CỦA BẠN
+
+N8N_WEBHOOK_URL = "https://n8n.n2nai.io/webhook-test/e7464a38-9ad7-4c7b-94b3-0e1ce1e2015e" # THAY THẾ XXXXXX BẰNG URL CỦA BẠN
 
 # Nếu bạn có dùng API Key trong Webhook của n8n (phần "Authentication"), thì điền vào đây
 # Nếu chưa dùng, cứ để rỗng hoặc xóa dòng này đi để đơn giản cho lần đầu
@@ -13,7 +14,7 @@ N8N_WEBHOOK_URL = "https://n8n.n2nai.io/webhook-test/64c7e9e9-5e9c-4ed7-b161-df2
 # --- KẾT THÚC THÔNG TIN CẦN THAY ĐỔI ---
 
 
-st.title("🐟 App Nhận Diện Bệnh Cá 🐟")
+st.title("App Nhận Diện Bệnh Cá 🐟")
 
 # Cho phép người dùng tải ảnh lên
 uploaded_file = st.file_uploader("Tải ảnh cá lên đây", type=["jpg", "jpeg", "png"])
