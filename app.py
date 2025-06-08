eafdc84304f727cf3b6b9e093c96b962e20a8dc6
Yennanng <yennan275@gmail.com>
2025-06-08 07:30:42 +0700

    ô

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import io
 
 # --- THÔNG TIN CẦN THAY ĐỔI ---
 # Dán Webhook Test URL mà bạn đã sao chép từ n8n ở Bước 2 vào đây
-N8N_WEBHOOK_URL = "https://n8n.n2nai.io/webhook-test/585b9b1f-542f-421b-8a00-3ef5be605275" # THAY THẾ XXXXXX BẰNG URL CỦA BẠN
+N8N_WEBHOOK_URL = "https://n8n.n2nai.io/webhook/3020bb98-bbef-4302-b3d4-0903c1031192" # THAY THẾ XXXXXX BẰNG URL CỦA BẠN
 
 # Nếu bạn có dùng API Key trong Webhook của n8n (phần "Authentication"), thì điền vào đây
 # Nếu chưa dùng, cứ để rỗng hoặc xóa dòng này đi để đơn giản cho lần đầu
